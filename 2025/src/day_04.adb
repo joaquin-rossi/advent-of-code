40d9d9055e6d70193a88ddceeadee2955c26d030
Joaquín Rossi <joaquin@joaquinrossi.net>
2025-12-04 22:16:52 -0300

    formateo

--- 2025/src/day_04.adb
+++ 2025/src/day_04.adb
@@ -1,51 +1,53 @@
-with Ada.Containers.Vectors; use Ada.Containers;
-with Ada.Numerics.Big_Numbers.Big_Integers; use Ada.Numerics.Big_Numbers.Big_Integers;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Containers.Vectors;
+with Ada.Numerics.Big_Numbers.Big_Integers;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
+
+use Ada.Containers;
+use Ada.Numerics.Big_Numbers.Big_Integers;
+use Ada.Strings.Unbounded;
+use Ada.Text_IO;
 
 procedure Day_04 is
-    package Cell_Vec is new Vectors
-        (Index_Type => Positive,
-         Element_Type => Boolean);
+    package Cell_Vec is new
+       Vectors (Index_Type => Positive, Element_Type => Boolean);
     use Cell_Vec;
 
-    package Grid_Vec is new Vectors
-        (Index_Type => Positive,
-         Element_Type => Cell_Vec.Vector);
+    package Grid_Vec is new
+       Vectors (Index_Type => Positive, Element_Type => Cell_Vec.Vector);
     use Cell_Vec;
 
     Width  : Integer := 0;
     Height : Integer := 0;
-    Grid : Grid_Vec.Vector := Grid_Vec.Empty_Vector;
+    Grid   : Grid_Vec.Vector := Grid_Vec.Empty_Vector;
 
-    function Get(X : Integer; Y : Integer) return Boolean is
+    function Get (X : Integer; Y : Integer) return Boolean is
     begin
         if not (X in 1 .. Width and Y in 1 .. Height) then
             return False;
         end if;
 
-        return Grid(Y)(X);
+        return Grid (Y) (X);
     end;
 
     Silver : Big_Integer := 0;
     Gold   : Big_Integer := 0;
 
     procedure Silver_Count is
-        X : Integer;
-        Y : Integer;
-        Xp : Integer;
-        Yp : Integer;
+        X     : Integer;
+        Y     : Integer;
+        Xp    : Integer;
+        Yp    : Integer;
         Neigh : Integer;
     begin
         for Y in 1 .. Height loop
             for X in 1 .. Width loop
-                if Get(X, Y) then
+                if Get (X, Y) then
                     Neigh := 0;
 
                     for Yp in -1 .. 1 loop
                         for Xp in -1 .. 1 loop
-                            if not (Xp = 0 and Yp = 0) and
-                                Get(X + Xp, Y + Yp)
+                            if not (Xp = 0 and Yp = 0) and Get (X + Xp, Y + Yp)
                             then
                                 Neigh := Neigh + 1;
                             end if;
@@ -61,13 +63,13 @@ procedure Day_04 is
     end;
 
     procedure Gold_Count is
-        X : Integer;
-        Y : Integer;
-        Xp : Integer;
-        Yp : Integer;
+        X     : Integer;
+        Y     : Integer;
+        Xp    : Integer;
+        Yp    : Integer;
         Neigh : Integer;
 
-        Grid_New : Grid_Vec.Vector;
+        Grid_New     : Grid_Vec.Vector;
         Grid_Changed : Boolean := True;
     begin
         while Grid_Changed loop
@@ -76,13 +78,13 @@ procedure Day_04 is
 
             for Y in 1 .. Height loop
                 for X in 1 .. Width loop
-                    if Get(X, Y) then
+                    if Get (X, Y) then
                         Neigh := 0;
 
                         for Yp in -1 .. 1 loop
                             for Xp in -1 .. 1 loop
-                                if not (Xp = 0 and Yp = 0) and
-                                    Get(X + Xp, Y + Yp)
+                                if not (Xp = 0 and Yp = 0)
+                                   and Get (X + Xp, Y + Yp)
                                 then
                                     Neigh := Neigh + 1;
                                 end if;
@@ -92,7 +94,7 @@ procedure Day_04 is
                         if Neigh < 4 then
                             Gold := Gold + 1;
                             Grid_Changed := True;
-                            Grid_New(Y)(X) := False;
+                            Grid_New (Y) (X) := False;
                         end if;
                     end if;
                 end loop;
@@ -102,12 +104,12 @@ procedure Day_04 is
         end loop;
     end;
 
-    Line   : Unbounded_String;
+    Line : Unbounded_String;
 begin
     while not End_Of_File loop
         Line := To_Unbounded_String (Get_Line);
         if Width = 0 then
-            Width := Length(Line);
+            Width := Length (Line);
         end if;
         Height := Height + 1;
 
@@ -116,22 +118,21 @@ begin
             Row : Cell_Vec.Vector := Cell_Vec.Empty_Vector;
         begin
             for I in 1 .. Width loop
-                Cell_Vec.Append(
-                    Row,
-                    (case Element(Line, I) is
-                        when '@' => True,
-                        when '.' => False,
-                        when others => raise Constraint_Error)
-                );
+                Cell_Vec.Append
+                   (Row,
+                    (case Element (Line, I) is
+                        when '@'    => True,
+                        when '.'    => False,
+                        when others => raise Constraint_Error));
             end loop;
 
-            Grid_Vec.Append(Grid, Row);
+            Grid_Vec.Append (Grid, Row);
         end;
     end loop;
 
     Silver_Count;
-    Gold_Count;
-
     Put_Line ("Silver: " & Silver'Image);
+
+    Gold_Count;
     Put_Line ("Gold: " & Gold'Image);
 end;
